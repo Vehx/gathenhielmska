a6d6b27ebc08ab43afe6ba420323975a5414ed07
vehx <delta222@gmail.com>
2020-03-31 15:36:06 +0200

    added enqueues for script and style

--- wordplate/public/themes/wordplate/functions.php
+++ wordplate/public/themes/wordplate/functions.php
@@ -26,6 +26,11 @@ add_action('after_setup_theme', function () {
         'widgets',
     ]);
 
+    add_action('wp_enqueue_scripts', function () {
+        wp_enqueue_style('customstyle', get_template_directory_uri() . '/assets/styles/app.css');
+        wp_enqueue_script('javascript', get_template_directory_uri() . '/assets/scripts/app.js');
+    });
+
     // Register navigation menus.
     register_nav_menus([
         'navigation' => __('Navigation', 'wordplate'),
