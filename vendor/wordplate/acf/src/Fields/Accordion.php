5e02a115e8146aa033c53724b32af5c5361a6e58
Daniel Thorsen <daniel.rickard.thorsen@gmail.com>
2020-03-31 23:49:17 +0200

    Setup CPT and installed ACF

---
+++ vendor/wordplate/acf/src/Fields/Accordion.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Copyright (c) Vincent Klaiber.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @see https://github.com/wordplate/acf
+ */
+
+declare(strict_types=1);
+
+namespace WordPlate\Acf\Fields;
+
+use WordPlate\Acf\Fields\Attributes\Endpoint;
+use WordPlate\Acf\Fields\Attributes\Instructions;
+
+class Accordion extends Field
+{
+    use Endpoint;
+    use Instructions;
+
+    /**
+     * @var string
+     */
+    protected $type = 'accordion';
+
+    public function multiExpand(): self
+    {
+        $this->config->set('multi_expand', true);
+
+        return $this;
+    }
+
+    public function open(): self
+    {
+        $this->config->set('open', true);
+
+        return $this;
+    }
+}
