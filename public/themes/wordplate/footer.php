e4ca923554c831dd2cee6ba529411cb32cdec5be
vehx <delta222@gmail.com>
2020-04-02 17:37:17 +0200

    added various css

--- public/themes/wordplate/footer.php
+++ public/themes/wordplate/footer.php
@@ -1,35 +1,34 @@
-  <?php wp_footer(); ?>
+<footer class="footer">
+    <div class="footer__main-text">
+        VÄLKOMMEN ATT KONTAKTA OSS VID EVENTUELLA FRÅGOR
+    </div>
+    <div class="footer__contact-list">
+        <div class="footer__contact">
+            <div class="footer__contact-info-box">
+                <div class="footer__contact-name">ISABEL LAGOS</div>
+                <div class="footer__contact-title">PROJEKTLEDARE & <br>KONSTNÄRLIG LEDARE</div>
+            </div>
+            <div class="footer__contact-info"> isabel@historieverket.se <br>0769-21 11 07</div>
+        </div>
+        <div class="footer__contact">
+            <div class="footer__contact-info-box">
+                <div class="footer__contact-name">JOEL HEIRÅS</div>
+                <div class="footer__contact-title">PROJEKTLEDARE & <br>KONSTNÄRLIG LEDARE</div>
+            </div>
+            <div class="footer__contact-info">joe.heiras@gmail.com <br>0760-98 38 32</div>
+        </div>
+        <div class="footer__contact">
+            <div class="footer__contact-info-box">
+                <div class="footer__contact-name">DAVID SPELING <br>BOLANDER</div>
+                <div class="footer__contact-title">PROJEKTLEDARE</div>
+            </div>
+            <div class="footer__contact-info">sperlingbolander@gmail.com <br>0707-30 51 34</div>
+        </div>
+        <div class="footer__contact-booking">gathenhielmskabokning@gmail.com</div>
+    </div>
+    <div class="footer__logo">LOGO GATHENHIELMSKA</div>
+</footer>
+<?php wp_footer(); ?>
+</body>
 
-  <footer class="footer">
-      <div class="footer__main-text">
-          VÄLKOMMEN ATT KONTAKTA OSS VID EVENTUELLA FRÅGOR
-      </div>
-      <div class="footer__contact-list">
-          <div class="footer__contact">
-              <div class="footer__contact-name">ISABEL LAGOS</div>
-              <div class="footer__contact-info-box">
-                  <div class="footer__contact-title">PROJEKTLEDARE & <br>KONSTNÄRLIG LEDARE</div>
-                  <div class="footer__contact-info"> isabel@historieverket.se <br>0769-21 11 07</div>
-              </div>
-          </div>
-          <div class="footer__contact">
-              <div class="footer__contact-name">JOEL HEIRÅS</div>
-              <div class="footer__contact-info-box">
-                  <div class="footer__contact-title">PROJEKTLEDARE & <br>KONSTNÄRLIG LEDARE</div>
-                  <div class="footer__contact-info">joe.heiras@gmail.com <br>0760-98 38 32</div>
-              </div>
-          </div>
-          <div class="footer__contact">
-              <div class="footer__contact-name">DAVID SPELING <br>BOLANDER</div>
-              <div class="footer__contact-info-box">
-                  <div class="footer__contact-title">PROJEKTLEDARE</div>
-                  <div class="footer__contact-info">sperlingbolander@gmail.com <br>0707-30 51 34</div>
-              </div>
-          </div>
-          <div class="footer__contact-booking">gathenhielmskabokning@gmail.com</div>
-          <div class="footer__logo">GATHENHIELMSKA</div>
-      </div>
-  </footer>
-  </body>
-
-  </html>
+</html>
