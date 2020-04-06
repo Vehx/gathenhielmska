1eb6f3dc852c1abb32e2a17b1b35b90f5fce1ec0
vehx <delta222@gmail.com>
2020-04-06 22:57:30 +0200

    added html, google map link and logos

--- public/themes/wordplate/footer.php
+++ public/themes/wordplate/footer.php
@@ -1,34 +1,46 @@
 <footer class="footer">
-    <div class="footer__main-text">
-        VÄLKOMMEN ATT KONTAKTA OSS VID EVENTUELLA FRÅGOR
+    <div class="footer__top-wrapper">
+        <div class="footer__logo">
+            <img src="<?php echo get_template_directory_uri() . "/assets/images/FooterLogo.svg" ?>" alt="Gathenhielmska logo" loading="lazy">
+            GATHENHIELMSKA
+        </div>
     </div>
-    <div class="footer__contact-list">
-        <div class="footer__contact">
-            <div class="footer__contact-info-box">
-                <div class="footer__contact-name">ISABEL LAGOS</div>
-                <div class="footer__contact-title">PROJEKTLEDARE & <br>KONSTNÄRLIG LEDARE</div>
-            </div>
-            <div class="footer__contact-info"> isabel@historieverket.se <br>0769-21 11 07</div>
+    <div class="footer__center-wrapper">
+        <div class="footer__newsletter">
+            <h3 class="footer__newsletter-text">Prenumerera på vårt nyhetsbrev</h3>
+            <p>Mailadress</p>
+            <form class="footer__newsletter-form" action="/" method="post">
+                <input type="email" placeholder="name@mail.com"><button type="submit">SKICKA</button>
+            </form>
         </div>
-        <div class="footer__contact">
-            <div class="footer__contact-info-box">
-                <div class="footer__contact-name">JOEL HEIRÅS</div>
-                <div class="footer__contact-title">PROJEKTLEDARE & <br>KONSTNÄRLIG LEDARE</div>
+        <div class="footer__information">
+            <div class="footer__directions">
+                <h3>Hitta hit</h3>
+                <p>Stigbergstorget 7</p>
+                <p>414 63 Göteborg</p>
+                <!-- todo add google maps link -->
+                <a class="footer__map" href="https://goo.gl/maps/z3aKagMzZx34MQQx5" target="_blank">
+                    <img src="<?php echo get_template_directory_uri() . "/assets/images/MapMarker.svg" ?>" alt="Map marker" loading="lazy">
+                    <p>karta</p>
+                </a>
             </div>
-            <div class="footer__contact-info">joe.heiras@gmail.com <br>0760-98 38 32</div>
-        </div>
-        <div class="footer__contact">
-            <div class="footer__contact-info-box">
-                <div class="footer__contact-name">DAVID SPELING <br>BOLANDER</div>
-                <div class="footer__contact-title">PROJEKTLEDARE</div>
+            <div class="footer__contact">
+                <div class="footer__contact-number">0737-56 67 78</div>
+                <div class="footer__contact-email">hej@gathenhielmska.se</div>
             </div>
-            <div class="footer__contact-info">sperlingbolander@gmail.com <br>0707-30 51 34</div>
         </div>
-        <div class="footer__contact-booking">gathenhielmskabokning@gmail.com</div>
     </div>
-    <div class="footer__logo">
-        <img src="<?php echo get_template_directory_uri() . "/assets/images/FooterLogo.svg" ?>" alt="Gathenhielmska logo" loading="lazy">
-        GATHENHIELMSKA
+    <div class="footer__bottom-wrapper">
+        <div class="footer__socials">
+            <a href="http://twitter.com">
+                <img src="<?php echo get_template_directory_uri() . "/assets/images/TwitterLogo.svg" ?>" alt="Gathenhielmska twitter" loading="lazy"></a>
+            <a href="http://facebook.com">
+                <img src="<?php echo get_template_directory_uri() . "/assets/images/FacebookLogo.svg" ?>" alt="Gathenhielmska facebook" loading="lazy"></a>
+            <a href="http://youtube.com">
+                <img src="<?php echo get_template_directory_uri() . "/assets/images/YoutubeLogo.svg" ?>" alt="Gathenhielmska youtube" loading="lazy"></a>
+            <a href="http://instagram.com">
+                <img src="<?php echo get_template_directory_uri() . "/assets/images/InstagramLogo.svg" ?>" alt="Gathenhielmska instagram" loading="lazy"></a>
+        </div>
     </div>
 </footer>
 <?php wp_footer(); ?>
