8210ae5826e1cea1c1a0a2bd644399c2ed482ad1
Daniel Thorsen <daniel.rickard.thorsen@gmail.com>
2020-04-06 16:38:48 +0200

    Looped out CPT

--- public/themes/wordplate/archive.php
+++ public/themes/wordplate/archive.php
@@ -6,63 +6,62 @@
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since Twenty Seventeen 1.0
+ * @subpackage Wordplate
  * @version 1.0
  */
 
 get_header(); ?>
 
+<!-- Start the Loop. -->
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+        <!-- Test if the current post is in category 3. -->
+        <!-- If it is, the div box is given the CSS class "post-cat-three". -->
+        <!-- Otherwise, the div box is given the CSS class "post". -->
+
+        <?php if (in_category('3')) : ?>
+            <div class="post-cat-three">
+            <?php else : ?>
+                <div class="post">
+                <?php endif; ?>
 
 
-<?php if (have_posts()) : ?>
-    <header class="page-header">
+                <!-- Display the Title as a link to the Post's permalink. -->
+
+                <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+
+
+                <!-- Display the date (November 16th, 2009 format) and a link to other posts by this posts author. -->
+
+                <small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small>
+
+
+                <!-- Display the Post's content in a div box. -->
+
+                <div class="entry">
+                    <?php the_content(); ?>
+                </div>
+
+
+                <!-- Display a comma separated list of the Post's Categories. -->
+
+                <p class="postmetadata"><?php _e('Posted in'); ?> <?php the_category(', '); ?></p>
+                </div> <!-- closes the first div box -->
+
+
+                <!-- Stop The Loop (but note the "else:" - see next line). -->
+
+            <?php endwhile;
+    else : ?>
+
+
+            <!-- The very first "if" tested to see if there were any Posts to -->
+            <!-- display.  This "else" part tells what do if there weren't any. -->
+            <p><?php esc_html_e('Sorry, no posts matched your criteria.'); ?></p>
+
+
+            <!-- REALLY stop The Loop. -->
+        <?php endif; ?>
+
         <?php
-        the_archive_title('<h1 class="page-title">', '</h1>');
-        the_archive_description('<div class="taxonomy-description">', '</div>');
-        ?>
-    </header><!-- .page-header -->
-<?php endif; ?>
-
-
-<main class="site-main" role="main">
-    <?php
-    if (have_posts()) :
-    ?>
-    <?php
-        // Start the Loop.
-        while (have_posts()) :
-            the_post();
-
-            /*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that
-				 * will be used instead.
-				 */
-            get_template_part('template-parts/post/content', get_post_format());
-
-        endwhile;
-
-        the_posts_pagination(
-            array(
-                'prev_text'          =>  '<span>' .  'previous' . '</span>',
-                'next_text'          => '<span>' . 'next' . '</span>',
-                'before_page_number' => '<span>' . __('Page', 'wordplate') . ' </span>',
-            )
-        );
-
-    else :
-
-        get_template_part('template-parts/post/content', 'none');
-
-    endif;
-    ?>
-
-</main><!-- #main -->
-
-
-
-
-<?php
-get_footer();
+        get_footer();
