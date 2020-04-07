59947460a710deab31972b36b2fb7e3e8758e26c
Viktor Sjöblom <Viktorsjoblom93@gmail.com>
2020-04-07 16:51:38 +0200

    changed design on menu and header

--- public/themes/wordplate/header.php
+++ public/themes/wordplate/header.php
@@ -15,10 +15,110 @@
                 <img src="<?php echo get_template_directory_uri() . "/assets/images/logo.svg" ?>" alt="Gathenhielmska logo" loading="lazy">
                 <p>GATHENHIELMSKA</p>
             </div>
-            <div class="header-lang">
-                <a href="#">SV</a>
-                <a href="#">EN</a>
-                <img src="<?php echo get_template_directory_uri() . "/assets/images/search.svg" ?>" alt="Search" loading="lazy">
+        </div>
+
+
+        <div class="open-menu hidden">
+            <div class="menu-links">
+                <div class="menu-event">
+                    <p>Event</p>
+                    <a href="#" class="to-event"><img src="<?php echo get_template_directory_uri() . "/assets/images/arrow.svg" ?>" alt="Arrow" loading="lazy"></a>
+                </div>
+                <div class="menu-event-expand hidden">
+                    <a href="#">
+                        <p>KURSER</p>
+                    </a>
+                    <a href="#">
+                        <p>KALENDER</p>
+                    </a>
+                    <a href="#">
+                        <p>GUIDAD VISNING</p>
+                    </a>
+                    <a href="#">
+                        <p>EVENEMANG</p>
+                    </a>
+                </div>
+                <div class="menu-about">
+                    <p>Om oss</p>
+                    <a href="#" class="to-about"><img src="<?php echo get_template_directory_uri() . "/assets/images/arrow.svg" ?>" alt="Arrow" loading="lazy"></a>
+                </div>
+                <div class="menu-about-expand hidden">
+                    <a href="#">
+                        <p>HISTORIA</p>
+                    </a>
+                    <a href="#">
+                        <p>GUIDAD VISNING</p>
+                    </a>
+                </div>
+                <div class="menu-archive">
+                    <p>Arkiv</p>
+                    <a href="#" class="to-archive"><img src="<?php echo get_template_directory_uri() . "/assets/images/arrow.svg" ?>" alt="Arrow" loading="lazy"></a>
+                </div>
+                <div class="menu-archive-expand hidden">
+                    <a href="#">
+                        <p>EVENEMANG</p>
+                    </a>
+                    <a href="#">
+                        <p>BILDER & FOTON</p>
+                    </a>
+                    <a href="#">
+                        <p>FILM</p>
+                    </a>
+                </div>
+                <div class="menu-contact">
+                    <p>Kontakt</p>
+                    <a href="#" class="to-contact"><img src="<?php echo get_template_directory_uri() . "/assets/images/arrow.svg" ?>" alt="Arrow" loading="lazy"></a>
+                </div>
+                <div class="menu-contact-expand hidden">
+                    <a href="#">
+                        <p>KONTAKTUPPGIFTER</p>
+                    </a>
+                    <a href="#">
+                        <p>MEDARBETANDE</p>
+                    </a>
+
+                </div>
+                <div class="menu-lang">
+                    <div class="menu-swedish">
+                        <a href="#"><img src="<?php echo get_template_directory_uri() . "/assets/images/swe.svg" ?>" alt="Swedish flag" loading="lazy">
+                            <p>Sv</p>
+                        </a>
+                    </div>
+                    <div class="menu-english">
+                        <a href="#"><img src="<?php echo get_template_directory_uri() . "/assets/images/eng.svg" ?>" alt="English flag" loading="lazy">
+                            <p>En</p>
+                        </a>
+                    </div>
+
+                </div>
+                <div class="menu-login-buttons">
+                    <button aria-label="login" class="menu-login">LOGGA IN</button>
+                    <button aria-label="create-account" class="menu-create-account">SKAPA KONTO</button>
+                </div>
+
+                <hr class="menu-line">
+                <div class="menu-search-field">
+                    <input type="text" placeholder="Sökord" class="menu-search">
+                    <button aria-label="search" class="menu-search-button">SÖK</button>
+                </div>
+
+                <div class="menu-social-media">
+                    <a href="#"><img src="<?php echo get_template_directory_uri() . "/assets/images/twitter.svg" ?>" alt="Twitter icon" loading="lazy">
+                        <a href="#"><img src="<?php echo get_template_directory_uri() . "/assets/images/facebook.svg" ?>" alt="Facebook icon" loading="lazy">
+                            <a href="#"><img src="<?php echo get_template_directory_uri() . "/assets/images/youtube.svg" ?>" alt="Youtube icon" loading="lazy">
+                                <a href="#"><img src="<?php echo get_template_directory_uri() . "/assets/images/instagram.svg" ?>" alt="Instagram icon" loading="lazy">
+                </div>
+
+                <div class="menu-newsletter">
+                    <p>Prenumerera på nyhetsbrevet</p>
+                    <input type="email" placeholder="skriv din e-postadress">
+                    <button aria-label="subscribe" class="menu-subscribe-button">PRENUMERERA</button>
+                </div>
+
+                <button aria-label=“close” class="menu-cancel"><img src="<?php echo get_template_directory_uri() . "/assets/images/cross.svg" ?>" alt="White cross" loading="lazy"></button>
             </div>
         </div>
+
+        <button aria-label="open" class="thumburger-menu">=</button>
+
     </header>
--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,3 +1,5 @@
+/* Thumburger menu */
+
 const thumbMenu = document.querySelector(".thumburger-menu");
 const openMenu = document.querySelector(".open-menu");
 const cancelMenu = document.querySelector(".menu-cancel");
@@ -9,41 +11,44 @@ const eventExpand = document.querySelector(".menu-event-expand");
 const aboutExpand = document.querySelector(".menu-about-expand");
 const archiveExpand = document.querySelector(".menu-archive-expand");
 const contactExpand = document.querySelector(".menu-contact-expand");
+const headerContainer = document.querySelector(".header-container");
 
-thumbMenu.addEventListener("click", (e) => {
+thumbMenu.addEventListener("click", e => {
   openMenu.classList.remove("hidden");
   thumbMenu.classList.add("hidden");
+  headerContainer.classList.add("hidden");
 });
 
-cancelMenu.addEventListener("click", (e) => {
+cancelMenu.addEventListener("click", e => {
   openMenu.classList.add("hidden");
   thumbMenu.classList.remove("hidden");
+  headerContainer.classList.remove("hidden");
 });
 
-goToEvent.addEventListener("click", (e) => {
-  eventExpand.classList.toggle("hidden");
-  aboutExpand.classList.add("hidden");
-  archiveExpand.classList.add("hidden");
-  contactExpand.classList.add("hidden");
+goToEvent.addEventListener("click", e => {
+  eventExpand.classList.toggle("flex");
+  aboutExpand.classList.remove("flex");
+  archiveExpand.classList.remove("flex");
+  contactExpand.classList.remove("flex");
 });
 
-goToAbout.addEventListener("click", (e) => {
-  aboutExpand.classList.toggle("hidden");
-  eventExpand.classList.add("hidden");
-  archiveExpand.classList.add("hidden");
-  contactExpand.classList.add("hidden");
+goToAbout.addEventListener("click", e => {
+  aboutExpand.classList.toggle("flex");
+  eventExpand.classList.remove("flex");
+  archiveExpand.classList.remove("flex");
+  contactExpand.classList.remove("flex");
 });
 
-goToArchive.addEventListener("click", (e) => {
-  archiveExpand.classList.toggle("hidden");
-  aboutExpand.classList.add("hidden");
-  eventExpand.classList.add("hidden");
-  contactExpand.classList.add("hidden");
+goToArchive.addEventListener("click", e => {
+  archiveExpand.classList.toggle("flex");
+  aboutExpand.classList.remove("flex");
+  eventExpand.classList.remove("flex");
+  contactExpand.classList.remove("flex");
 });
 
-goToContact.addEventListener("click", (e) => {
-  contactExpand.classList.toggle("hidden");
-  archiveExpand.classList.add("hidden");
-  aboutExpand.classList.add("hidden");
-  eventExpand.classList.add("hidden");
+goToContact.addEventListener("click", e => {
+  contactExpand.classList.toggle("flex");
+  archiveExpand.classList.remove("flex");
+  aboutExpand.classList.remove("flex");
+  eventExpand.classList.remove("flex");
 });
