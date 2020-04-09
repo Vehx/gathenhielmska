7f7c87465faadb13e036250ffa50783c9100b97c
vehx <delta222@gmail.com>
2020-04-09 18:19:40 +0200

    added timeline structure

--- public/themes/wordplate/page-historia.php
+++ public/themes/wordplate/page-historia.php
@@ -1,47 +1,71 @@
 <?php get_header(); ?>
 
-<main role="main">
-    <div class="test1"></div>
-    <div class="test2">
-        <div class="history__text">
-            Vid Stigbergstorget ligger det så kallade Gathenhielmska huset, som ofta förknippas med kaparkaptenen Lars Gathenhielm, som för Karl XII:s räkning uppbringade danska skepp under det stora nordiska kriget i början av 1700-talet.
-        </div>
-        <div>
-            <img src="" alt="Anna Dahlström" loading="lazy">
-            Huset uppfördes emellertid långt efter Lars Gathenhielms död av hans svåger kommissarien Johan Busck sannolikt mellan åren 1744 och 1747. Busck hade köpt ”Gamla reparebanan” med alla därpå befintliga hus och pannor samt ”gården, Lieutenant Collins benämnd”, av sin brorson Anders Gathenhielm.
-        </div>
-        <div>
-            Renoveringen genomfördes av konstnären Johan Nilsson, make till ägaren medicine doktor Anna Nilsson. Johan Buscks arvingar sålde huset på offentlig auktion till superkargören Johan Petter Tranchell år 1816.
-        </div>
-        <div>
-            Tranchell hade varit i Ostindiska kompaniets tjänst och gjort flera resor till Kina. När Ostindiska kompaniet avvecklades 1813, ägnade han sig åt affärsverksamhet i Göteborg. Tranchell sålde år 1824 huset till bruksägaren Fredrik Ulrik von Aken.
-        </div>
-        <div>
-            År 1846 köpte repslagaren Mathias Nicolaus Dahlström fastigheten av von Aken för 20.000 riksdaler.
-        </div>
-        <div>
-            När Dahlströms änka Eva Johanna avled år 1867, ärvdes fastigheten av deras två barn, Johan Niklas och Johanna Augusta, som var gift med repslagerifabrikören Charles Åhmansson. År 1874 blev Johan Niklas, som då var jurist och hovrättsnotarie ensam ägare till huset.
-        </div>
-        <div>
-            Sedan han avlidit år 1891 och hans hustru gått bort 1904, blev deras enda barn medicine doktor Anna Dahlström ensam ägare till huset.
-        </div>
+<main role="main" class="history">
+    <div class="history__title-box">Historia</div>
+    <div class="history__breadcrumbs">HEM > OM OSS > HISTORIA</div>
+    <div class="history__intro-text">
+        Vid Stigbergstorget ligger det så kallade Gathenhielmska huset, som ofta förknippas med kaparkaptenen Lars Gathenhielm, som för Karl XII:s räkning uppbringade danska skepp under det stora nordiska kriget i början av 1700-talet.
+    </div>
+    <section class="history__timeline">
+        <div class="history__timeline-line">
+            <div class="history__timeline-line-pre"></div>
+            <div class="history__timeline-line-year">~1745</div>
+            <div class="history__timeline-line-one"></div>
+            <div class="history__timeline-line-year">1816</div>
+            <div class="history__timeline-line-two"></div>
+            <div class="history__timeline-line-year">1824</div>
+            <div class="history__timeline-line-three"></div>
+            <div class="history__timeline-line-year">1846</div>
+            <div class="history__timeline-line-four"></div>
+            <div class="history__timeline-line-year">1867</div>
+            <div class="history__timeline-line-five"></div>
+            <div class="history__timeline-line-year">1891</div>
+            <div class="history__timeline-line-six"></div>
+            <div class="history__timeline-line-year">Anna Dahlström</div>
+            <div class="history__timeline-line-seven"></div>
+            <div class="history__timeline-line-year">1941</div>
+            <div class="history__timeline-line-eight"></div>
+            <div class="history__timeline-line-year">1987</div>
+            <div class="history__timeline-line-nine"></div>
 
-        <div>
-            <div>
-                <img src="" alt="Anna Dahlström" loading="lazy">
-                Anna Dahlström var född år 1867 och kom att utbilda sig till ögonläkare. Hon blev medicinelicentiat år 1899. Hon etablerade sig sedan till privatpraktiserande ögonläkare i Göteborg och hade praktik bland annat på Östra Hamngatan 36, men hade även mottagning i Gathenhielmska huset vardagsförmiddagar.
+        </div>
+        <div class="history__timeline-story">
+            <div class="history__timeline-story-one">
+                <span class="history__capital">H</span>uset uppfördes emellertid långt efter Lars Gathenhielms död av hans svåger kommissarien Johan Busck sannolikt mellan åren 1744 och 1747. Busck hade köpt ”Gamla reparebanan” med alla därpå befintliga hus och pannor samt ”gården, Lieutenant Collins benämnd”, av sin brorson Anders Gathenhielm.
+            </div>
+            <div class="history__timeline-story-two">
+                <!-- <img class="history__timeline-story-two-img" src="<?php echo get_template_directory_uri() . "/assets/images/huset-svartvit.png" ?>" alt="Gathenhielmska huset" loading="lazy"> -->
+                Renoveringen genomfördes av konstnären Johan Nilsson, make till ägaren medicine doktor Anna Nilsson. Johan Buscks arvingar sålde huset på offentlig auktion till superkargören Johan Petter Tranchell år 1816.
+            </div>
+            <div class="history__timeline-story-three">
+                Tranchell hade varit i Ostindiska kompaniets tjänst och gjort flera resor till Kina. När Ostindiska kompaniet avvecklades 1813, ägnade han sig åt affärsverksamhet i Göteborg. Tranchell sålde år 1824 huset till bruksägaren Fredrik Ulrik von Aken.
+            </div>
+            <div class="history__timeline-story-four">
+                År 1846 köpte repslagaren Mathias Nicolaus Dahlström fastigheten av von Aken för 20.000 riksdaler.
+            </div>
+            <div class="history__timeline-story-five">
+                När Dahlströms änka Eva Johanna avled år 1867, ärvdes fastigheten av deras två barn, Johan Niklas och Johanna Augusta, som var gift med repslagerifabrikören Charles Åhmansson. År 1874 blev Johan Niklas, som då var jurist och hovrättsnotarie ensam ägare till huset.
+            </div>
+            <div class="history__timeline-story-six">
+                Sedan han avlidit år 1891 och hans hustru gått bort 1904, blev deras enda barn medicine doktor Anna Dahlström ensam ägare till huset.
+            </div>
+            <div class="history__timeline-story-seven">
+                <div>
+                    Anna Dahlström
+                    <img src="<?php echo get_template_directory_uri() . "/assets/images/TwitterLogo.svg" ?>" alt="Anna Dahlström" loading="lazy">
+                    var född år 1867 och kom att utbilda sig till ögonläkare. Hon blev medicinelicentiat år 1899. Hon etablerade sig sedan till privatpraktiserande ögonläkare i Göteborg och hade praktik bland annat på Östra Hamngatan 36, men hade även mottagning i Gathenhielmska huset vardagsförmiddagar.
+                </div>
+            </div class="history__timeline-story-eight">
+            <div class="history__timeline-story-nine">
+                När Anna Dahlström dog år 1941, hade hon testamenterat fastigheten till sitt kusinbarn Vera Tham, som flyttade från Stockholm med sin familj och bosatte sig i huset. Hon ansökte år 1943 om att huset och trädgården skulle erhålla kulturminnesskydd. Trots att huset var privatbostad höll hon visningar av dess intressanta interiör i olika sammanhang. Åren 1969-1987 var Vera Thams barn Per-Vollrath och Louise 1941 Gathenhielmska husets ägare. De fullföljde under de sista årtiondena Vera Thams intentioner beträffande omvårdnad och husets tillgänglighet för specialvisningar fram till dess försäljning till Göteborgs stad.
+            </div>
+            <div class="history__timeline-story-ten">
+                Efter beslut i kommunfullmäktige år 1986 övertog Göteborgs stad huset med tillträde 1987.
             </div>
         </div>
-        <div>
-            När Anna Dahlström dog år 1941, hade hon testamenterat fastigheten till sitt kusinbarn Vera Tham, som flyttade från Stockholm med sin familj och bosatte sig i huset. Hon ansökte år 1943 om att huset och trädgården skulle erhålla kulturminnesskydd. Trots att huset var privatbostad höll hon visningar av dess intressanta interiör i olika sammanhang. Åren 1969-1987 var Vera Thams barn Per-Vollrath och Louise 1941 Gathenhielmska husets ägare. De fullföljde under de sista årtiondena Vera Thams intentioner beträffande omvårdnad och husets tillgänglighet för specialvisningar fram till dess försäljning till Göteborgs stad.
-        </div>
-        <div>
-            Efter beslut i kommunfullmäktige år 1986 övertog Göteborgs stad huset med tillträde 1987.
-        </div>
-
-        <div>
-            Fram till 1846 hade Gathenhielmska huset karaktären av ett högreståndsmässigt lantställe. Under senare delen av 1800-talet och en bit in på 1900-talet kom en nyttoinriktning att prägla huset med lägenheter och butiker. Eftersom stt Anna Dahlström var ekonomiskt oberoende, beslöt hon att kosta på sitt hus en grundlig restaurering. Genom Röhsska museets första chef Axel Nilsson fick hon kontakt med den konservatorsutbildade konstnären Johan Nilsson. Tillsammans med honom inleddes år 1916 arbetet med att i görligaste mån återställa huset i ursprungligt skick. År 1920 ingick Anna Dahlström och Johan Nilsson äktenskap. Renoveringen av huset blev deras gemensamma projekt. År 1933 hotades huset av rivning. Johan Nilsson engagerade sig med kraft i arbetet för att bevara huset. Han fick stöd av Sjöfartsmuseets dåvarande chef Olof Traung, Gamla Majpojkars förbund och Majornas kommunalförening. Kulturminnesföreningen Gathenhielm bildades. Även den legendariske chefredaktören Torgny Segerstedt engagerade sig i frågan. Protesterna medförde att planerna på att riva Gathenhielmska huset och jämna de övriga 1700-talsbyggnaderna i dess närhet stoppades. 1936 antog stadsfullmäktige en ny stadsplan, som innebar att huset räddades och förutsättningarna för att inrätta ett kulturreservat kom på plats. Redan år 1935 hade kulturminnesföreningen överenskommit med staden om underhåll och nyttjanderätt till husen i det som idag utgör Gathenhielmska reservatet. Johan Nilsson måste vid Anna Dahlströms frånfälle på grund av hennes testamente lämna sin bostad i Gathenhielmska huset. Han bereddes en hedersbostad i fastigheten Härbärget inne i reservatet. Under senare år sysslade han bland annat med att samla material till ett reservatmuseum, som han testamenterade till kulturminnesföreningen. Samlingarna är numera skingrade.
-        </div>
+    </section>
+    <div class="history__end-text">
+        <span class="history__capital">F</span>ram till 1846 hade Gathenhielmska huset karaktären av ett högreståndsmässigt lantställe. Under senare delen av 1800-talet och en bit in på 1900-talet kom en nyttoinriktning att prägla huset med lägenheter och butiker. Eftersom stt Anna Dahlström var ekonomiskt oberoende, beslöt hon att kosta på sitt hus en grundlig restaurering. Genom Röhsska museets första chef Axel Nilsson fick hon kontakt med den konservatorsutbildade konstnären Johan Nilsson. Tillsammans med honom inleddes år 1916 arbetet med att i görligaste mån återställa huset i ursprungligt skick. År 1920 ingick Anna Dahlström och Johan Nilsson äktenskap. Renoveringen av huset blev deras gemensamma projekt. År 1933 hotades huset av rivning. Johan Nilsson engagerade sig med kraft i arbetet för att bevara huset. Han fick stöd av Sjöfartsmuseets dåvarande chef Olof Traung, Gamla Majpojkars förbund och Majornas kommunalförening. Kulturminnesföreningen Gathenhielm bildades. Även den legendariske chefredaktören Torgny Segerstedt engagerade sig i frågan. Protesterna medförde att planerna på att riva Gathenhielmska huset och jämna de övriga 1700-talsbyggnaderna i dess närhet stoppades. 1936 antog stadsfullmäktige en ny stadsplan, som innebar att huset räddades och förutsättningarna för att inrätta ett kulturreservat kom på plats. Redan år 1935 hade kulturminnesföreningen överenskommit med staden om underhåll och nyttjanderätt till husen i det som idag utgör Gathenhielmska reservatet. Johan Nilsson måste vid Anna Dahlströms frånfälle på grund av hennes testamente lämna sin bostad i Gathenhielmska huset. Han bereddes en hedersbostad i fastigheten Härbärget inne i reservatet. Under senare år sysslade han bland annat med att samla material till ett reservatmuseum, som han testamenterade till kulturminnesföreningen. Samlingarna är numera skingrade.
     </div>
 </main>
 
