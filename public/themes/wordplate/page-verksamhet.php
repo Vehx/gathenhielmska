8a133f8d03e79bc8510f720c5e2c6cfa2a0504bf
vehx <delta222@gmail.com>
2020-04-11 17:59:54 +0200

    added files and styling

---
+++ public/themes/wordplate/page-verksamhet.php
@@ -0,0 +1,45 @@
+<?php get_header(); ?>
+
+<main role="main" class="operations">
+    <div class="operations__title-box">Verksamheter</div>
+    <div class="operations__breadcrumbs">
+        <a href="/">
+            <span class="breadcrumb-faded">HEM</span>
+        </a>
+        >
+        <a href="/omoss">
+            <span class="breadcrumb-faded">OM OSS</span>
+        </a>
+        >
+        <a href="/verksamhet">
+            <span class="breadcrumb-active">VERKSAMHETER</span>
+        </a>
+    </div>
+    <div class="operations__page-info"></div>
+    <div class="operations__intro-text">
+        <img src="<?php echo get_template_directory_uri() . "/assets/images/Staircase.png" ?>" alt="Trappa i Gathenhielmska huset" loading="lazy">
+        <div class="operations__text">
+            <span class="operations__capital">T</span>anken med verksamheten i Gathenhielmska Huset är att skapa processer som uppmuntrar innovativa möten och samverkan mellan konstnärer, kulturutövare, lokalt näringsliv och besökare. Gathenhielmska Huset skall vara en sammanlänkande plats i området och en arbetsplats och scen för design, dans, opera, musik, teater, konst och litteratur, med mera.
+        </div>
+    </div>
+    <section class="operations__content">
+        <p>
+            Arbetsplatser och intern samverkan
+        </p>
+        <p>
+            Idag är det runt tio aktörer som arbetar regelbundet i huset. Allt från design, teater, musik, och casting, till arkitektur, samtalsterapi och Podcasts.
+        </p>
+        <br>
+        <p>
+            Vi är måna om att skapa ett hållbart system för samverkan mellan olika aktörer. Vi tror att mångfalden gynnar både de som arbetar i huset och verksamheten i sin helhet. Olikheterna stärker en gemenskap där alla deltar utifrån sina förutsättningar. Varje aktör är en tillgång och en viktig del för helheten. Vi erbjuder en inkluderande och inspirerande miljö där alla är inbjudna att skapa eller ta del av skapandet och öppnar en plats utan begränsningar för mötet mellan historia, människa och konst.
+        </p>
+        <br>
+        <p>
+            Vi ser livet i och kring huset som ekosystem, en god grund för en samverkansmodell där vi tillsammans skapar en näringsrik mylla för samskapande, genom att verka för en hållbar och kollektiv gemenskap. Vi tror att en hållbar kollektiv social struktur, där vi stödjer och lär av varandra, är det bästa sättet att skapa ett sammanhang som kan förvalta Gathenhielmska husets värde.
+        </p>
+    </section>
+    <div class="operations__end-text">
+    </div>
+</main>
+
+<?php get_footer(); ?>
