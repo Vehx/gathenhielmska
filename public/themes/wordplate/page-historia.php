0ded26f9a95a301599865adb654362015f32eb9d
vehx <delta222@gmail.com>
2020-04-11 15:14:43 +0200

    added breadcrumb styling

--- public/themes/wordplate/page-historia.php
+++ public/themes/wordplate/page-historia.php
@@ -2,7 +2,20 @@
 
 <main role="main" class="history">
     <div class="history__title-box">Historia</div>
-    <div class="history__breadcrumbs">HEM > OM OSS > HISTORIA</div>
+    <div class="history__breadcrumbs">
+        <a href="/">
+            <span class="breadcrumb-faded">HEM</span>
+        </a>
+        >
+        <a href="/omoss">
+            <span class="breadcrumb-faded">OM OSS</span>
+        </a>
+        >
+        <a href="/historia">
+            <span class="breadcrumb-active">HISTORIA</span>
+        </a>
+    </div>
+    <div class="history__page-info"></div>
     <div class="history__intro-text">
         Vid Stigbergstorget ligger det så kallade Gathenhielmska huset, som ofta förknippas med kaparkaptenen Lars Gathenhielm, som för Karl XII:s räkning uppbringade danska skepp under det stora nordiska kriget i början av 1700-talet.
     </div>
